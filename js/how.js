60a6a06e3914ce2bba090a5124233645061574c1
bps <brandon.p.sullivan@gmail.com>
2019-04-11 17:10:54 -0500

    Undo TEST
    I wonder if GitHub Pages has a better way to test things in the environment. It is not 1:1 with my local, evidently.

--- js/how.js
+++ js/how.js
@@ -1,45 +1,5 @@
 // HOW
 (function ($) {
-
-	// MODAL.JS
-	const body         = $('body');
-	const modal        = $('#modal');
-	const modalTrigger = $('[data-modal]');
-	//const modalWrapper = $('.modal-wrapper');
-	const modalContent = $('.modal-content');
-
-	function closeModal() {
-		//modalWrapper.removeClass('active');
-		body.removeClass('modal-open');
-	}
-	function openModal(target) {
-		//target.addClass('active');
-		body.addClass('modal-open');
-	}
-
-	$('.modal-screen, .close-modal').click(function(){
-		closeModal();
-	});
-
-	$(document).keyup(function(e) {
-		if (e.keyCode === 27) closeModal();
-	});
-
-	function launchModal(trigger, callback) {
-		let href        = trigger.attr('href');
-		let location    = trigger.data('location');
-		let partialName = href.replace('#','');
-
-		// Dyamically load content from the /_partials folder
-		modalContent.load('_partials/' + location + '/' + partialName, function(){
-			openModal(modal);
-
-			// On successful load, run any callbacks to prevent dynamic loading issues
-			if (callback !== null) {
-				callback();
-			}
-		});
-	}
 	
 	// Presupposes the inclusion of both modal.js and glide.js
 	modalTrigger.click(function(e){
